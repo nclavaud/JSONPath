a83067fd76948b01a7320e58d535da0a67b979d3
nclavaud <nicolas@lrqdo.fr>
2013-10-21 13:02:01 +0200

    Add composer.json file and fix brackets in JsonPath.php

--- JsonPath.php
+++ JsonPath.php
@@ -6,11 +6,10 @@
  * Licensed under the MIT (MIT-LICENSE.txt) licence.
  */
 
-namespace Goessner\JsonPath;
+namespace Lrqdo\Infrastructure\JsonPath;
 
 class JsonPath
 {
-
     var $obj = null;
     var $resultType = "Value";
     var $result = array();
@@ -23,10 +22,11 @@ class JsonPath
         $this->obj = $obj;
         if ($expr && $obj && ($this->resultType == "VALUE" || $this->resultType == "PATH")) {
             $this->trace(preg_replace("/^\\$;/", "", $x), $obj, "$");
-            if (count($this->result))
+            if (count($this->result)) {
                 return $this->result;
-            else
+            } else {
                 return false;
+            }
         }
     }
 
@@ -37,6 +37,7 @@ class JsonPath
         $x = preg_replace(array("/'?\.'?|\['?/", "/;;;|;;/", "/;$|'?\]|'$/"), array(";", ";..;", ""), $x);
         $x = preg_replace_callback("/#([0-9]+)/", array(&$this, "_callback_02"), $x);
         $this->result = array();  // result array was temporarily used as a buffer ..
+
         return $x;
     }
 
@@ -54,15 +55,19 @@ class JsonPath
     {
         $x = explode(";", $path);
         $p = "$";
-        for ($i = 1, $n = count($x); $i < $n; $i++)
+        for ($i = 1, $n = count($x); $i < $n; $i++) {
             $p .= preg_match("/^[0-9*]+$/", $x[$i]) ? ("[" . $x[$i] . "]") : ("['" . $x[$i] . "']");
+        }
+
         return $p;
     }
 
     function store($p, $v)
     {
-        if ($p)
+        if ($p) {
             array_push($this->result, ($this->resultType == "PATH" ? $this->asPath($p) : $v));
+        }
+
         return !!$p;
     }
 
@@ -73,25 +78,28 @@ class JsonPath
             $loc = array_shift($x);
             $x = implode(";", $x);
 
-            if (is_array($val) && array_key_exists($loc, $val))
+            if (is_array($val) && array_key_exists($loc, $val)) {
                 $this->trace($x, $val[$loc], $path . ";" . $loc);
-            else if ($loc == "*")
+            } elseif ($loc == "*") {
                 $this->walk($loc, $x, $val, $path, array(&$this, "_callback_03"));
-            else if ($loc === "..") {
+            } elseif ($loc === "..") {
                 $this->trace($x, $val, $path);
                 $this->walk($loc, $x, $val, $path, array(&$this, "_callback_04"));
-            } else if (preg_match("/^\(.*?\)$/", $loc)) // [(expr)]
+            } elseif (preg_match("/^\(.*?\)$/", $loc)) { // [(expr)]
                 $this->trace($this->evalx($loc, $val, substr($path, strrpos($path, ";") + 1)) . ";" . $x, $val, $path);
-            else if (preg_match("/^\?\(.*?\)$/", $loc)) // [?(expr)]
+            } elseif (preg_match("/^\?\(.*?\)$/", $loc)) { // [?(expr)]
                 $this->walk($loc, $x, $val, $path, array(&$this, "_callback_05"));
-            else if (preg_match("/^(-?[0-9]*):(-?[0-9]*):?(-?[0-9]*)$/", $loc)) // [start:end:step]  phyton slice syntax
+            } elseif (preg_match("/^(-?[0-9]*):(-?[0-9]*):?(-?[0-9]*)$/", $loc)) { // [start:end:step]  phyton slice syntax
                 $this->slice($loc, $x, $val, $path);
-            else if (preg_match("/,/", $loc)) // [name1,name2,...]
-                for ($s = preg_split("/'?,'?/", $loc), $i = 0, $n = count($s); $i < $n; $i++)
+            } elseif (preg_match("/,/", $loc)) { // [name1,name2,...]
+                for ($s = preg_split("/'?,'?/", $loc), $i = 0, $n = count($s); $i < $n; $i++) {
                     $this->trace($s[$i] . ";" . $x, $val, $path);
+                }
+            }
         }
-        else
+        else {
             $this->store($path, $val);
+        }
     }
 
     function _callback_03($m, $l, $x, $v, $p)
@@ -101,20 +109,23 @@ class JsonPath
 
     function _callback_04($m, $l, $x, $v, $p)
     {
-        if (is_array($v[$m]))
+        if (is_array($v[$m])) {
             $this->trace("..;" . $x, $v[$m], $p . ";" . $m);
+        }
     }
 
     function _callback_05($m, $l, $x, $v, $p)
     {
-        if ($this->evalx(preg_replace("/^\?\((.*?)\)$/", "$1", $l), $v[$m]))
+        if ($this->evalx(preg_replace("/^\?\((.*?)\)$/", "$1", $l), $v[$m])) {
             $this->trace($m . ";" . $x, $v, $p);
+        }
     }
 
     function walk($loc, $expr, $val, $path, $f)
     {
-        foreach ($val as $m => $v)
+        foreach ($val as $m => $v) {
             call_user_func($f, $m, $loc, $expr, $val, $path);
+        }
     }
 
     function slice($loc, $expr, $v, $path)
@@ -126,22 +137,22 @@ class JsonPath
         $step = (int) $s[2] ? $s[2] : 1;
         $start = ($start < 0) ? max(0, $start + $len) : min($len, $start);
         $end = ($end < 0) ? max(0, $end + $len) : min($len, $end);
-        for ($i = $start; $i < $end; $i+=$step)
+        for ($i = $start; $i < $end; $i += $step) {
             $this->trace($i . ";" . $expr, $v, $path);
+        }
     }
 
-    function evalx($x, $v, $vname)
+    function evalx($x, $v, $vname = null)
     {
         $name = "";
         $expr = preg_replace(array("/\\$/", "/@/"), array("\$this->obj", "\$v"), $x);
         $res = eval("\$name = $expr;");
 
-        if ($res === FALSE)
+        if (false === $res) {
             print("(jsonPath) SyntaxError: " . $expr);
-        else
+        }
+        else {
             return $name;
+        }
     }
-
 }
-
-?>
